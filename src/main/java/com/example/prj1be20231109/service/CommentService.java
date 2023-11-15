9028a28be890c3eca8b6323d520fd3a37e1d5b71
sebaek <sebaek@gmail.com>
2023-11-15 10:40:55 +0900

    게시물번호로 댓글 목록 가져오기

--- src/main/java/com/example/prj1be20231109/service/CommentService.java
+++ src/main/java/com/example/prj1be20231109/service/CommentService.java
@@ -6,6 +6,8 @@ import com.example.prj1be20231109.mapper.CommentMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CommentService {
@@ -32,5 +34,9 @@ public class CommentService {
         }
 
         return true;
+    }
+
+    public List<Comment> list(Integer boardId) {
+        return mapper.selectByBoardId(boardId);
     }
 }
