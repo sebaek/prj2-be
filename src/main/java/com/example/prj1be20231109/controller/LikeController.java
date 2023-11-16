8443165e5cb545b8adb662fabcd81701a7a267da
sebaek <sebaek@gmail.com>
2023-11-16 16:21:10 +0900

    좋아요 기능

--- src/main/java/com/example/prj1be20231109/controller/LikeController.java
+++ src/main/java/com/example/prj1be20231109/controller/LikeController.java
@@ -1,12 +1,12 @@
 package com.example.prj1be20231109.controller;
 
 import com.example.prj1be20231109.domain.Like;
+import com.example.prj1be20231109.domain.Member;
 import com.example.prj1be20231109.service.LikeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +16,13 @@ public class LikeController {
     private final LikeService service;
 
     @PostMapping
-    public void like(@RequestBody Like like) {
-        service.update(like);
+    public ResponseEntity<Object> like(@RequestBody Like like,
+                                       @SessionAttribute(value = "login", required = false) Member login) {
+        if (login == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        service.update(like, login);
+        return null;
     }
 }
