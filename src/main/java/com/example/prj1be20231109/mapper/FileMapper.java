af3bd8814846e220d71c052855fe5a9e8db85aa2
sebaek <sebaek@gmail.com>
2023-11-20 15:52:23 +0900

    이미지 경로 응답

--- src/main/java/com/example/prj1be20231109/mapper/FileMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/FileMapper.java
@@ -2,6 +2,9 @@ package com.example.prj1be20231109.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface FileMapper {
@@ -11,4 +14,11 @@ public interface FileMapper {
         VALUES (#{boardId}, #{name})
         """)
     int insert(Integer boardId, String name);
+
+    @Select("""
+        SELECT name
+        FROM boardFile
+        WHERE boardId = #{boardId}
+        """)
+    List<String> selectNamesByBoardId(Integer boardId);
 }
